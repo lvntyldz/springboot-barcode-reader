917512ebc8cc73d29ea2b968e4c33b594c31940c
levent yıldız <lewenty@gmail.com>
2020-02-23 19:22:40 +0300

    output path updated

--- src/main/java/com/ba/barcodereader/props/Config.java
+++ src/main/java/com/ba/barcodereader/props/Config.java
@@ -6,6 +6,6 @@ public class Config {
     public static final String SCANNER_DEST_FOLDER = "C:\\twain";
     public static final String SCANNED_FILE_PATH = "C:\\twain\\img000001.jpg";
     public static final String SCANNER_EXE_PATH = "kodak\\kodak.scan.exe";
-    public static final String TARGET_DIR = "C:\\twain\\target\\";
+    public static final String TARGET_DIR = "target\\";
 
 }
