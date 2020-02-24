22356893245f060e32c1304f94fcba19ccc8ac49
levent yıldız <lewenty@gmail.com>
2020-02-24 17:06:52 +0300

    5 sec delay time added

--- src/main/java/com/ba/barcodereader/helper/ScannerHelper.java
+++ src/main/java/com/ba/barcodereader/helper/ScannerHelper.java
@@ -11,8 +11,8 @@ public class ScannerHelper {
     public static void scanFile() throws InterruptedException {
         try {
             Process process = new ProcessBuilder(Config.SCANNER_EXE_PATH).start();
-            process.waitFor();
-            process.destroy();
+            process.waitFor(5, TimeUnit.SECONDS);
+            new ProcessBuilder("taskkill /IM 'kodak.scan.exe' /F").start();
         } catch (FileNotFoundException e) {//TODO:Development[Throw custom exception]
             e.printStackTrace();//TODO:Add Logger
         } catch (IOException e) {
