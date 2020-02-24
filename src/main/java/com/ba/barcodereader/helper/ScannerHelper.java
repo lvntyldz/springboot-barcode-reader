7fbe9fe4c69b63eb89689831dc437c4caf10a827
levent yıldız <lewenty@gmail.com>
2020-02-24 16:52:02 +0300

    run process synchronously

--- src/main/java/com/ba/barcodereader/helper/ScannerHelper.java
+++ src/main/java/com/ba/barcodereader/helper/ScannerHelper.java
@@ -4,13 +4,15 @@ import com.ba.barcodereader.props.Config;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 public class ScannerHelper {
 
-    public static void scanFile() {
+    public static void scanFile() throws InterruptedException {
         try {
             Process process = new ProcessBuilder(Config.SCANNER_EXE_PATH).start();
-            //process.destroy();
+            process.waitFor();
+            process.destroy();
         } catch (FileNotFoundException e) {//TODO:Development[Throw custom exception]
             e.printStackTrace();//TODO:Add Logger
         } catch (IOException e) {
