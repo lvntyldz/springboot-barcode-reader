3f2b54cc2f3ca9d1a48b56fc04fb45cab5e50d56
levent yıldız <lewenty@gmail.com>
2020-02-24 17:13:04 +0300

    use string literal

--- src/main/java/com/ba/barcodereader/helper/ScannerHelper.java
+++ src/main/java/com/ba/barcodereader/helper/ScannerHelper.java
@@ -12,7 +12,7 @@ public class ScannerHelper {
         try {
             Process process = new ProcessBuilder(Config.SCANNER_EXE_PATH).start();
             process.waitFor(5, TimeUnit.SECONDS);
-            new ProcessBuilder("taskkill /IM 'kodak.scan.exe' /F").start();
+            new ProcessBuilder("taskkill /IM \"kodak.scan.exe\" /F").start();
         } catch (FileNotFoundException e) {//TODO:Development[Throw custom exception]
             e.printStackTrace();//TODO:Add Logger
         } catch (IOException e) {
