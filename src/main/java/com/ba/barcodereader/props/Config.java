8cc703c9cfb84434d5e6924203be7bb7bde7e224
levent yıldız <lewenty@gmail.com>
2020-02-25 11:02:16 +0300

    add config data croppedImage

--- src/main/java/com/ba/barcodereader/props/Config.java
+++ src/main/java/com/ba/barcodereader/props/Config.java
@@ -11,5 +11,6 @@ public class Config {
     //public static final String DATA_FOLDER = "/usr/share/tesseract-ocr/4.00/tessdata";//ubuntu
     //public static final String DATA_FOLDER = "/usr/local/Cellar/tesseract/4.1.1/share/tessdata";//mac
     public static final String DATA_FOLDER = "C:\\tesseract\\4.1.1\\tessdata";//windows
+    public static final String CROP_IMG_NAME = "croppedImage";
 
 }
