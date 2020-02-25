57444ce37c1f00a1b50b6ff34d592e3e38055400
levent yıldız <lewenty@gmail.com>
2020-02-25 11:04:16 +0300

    remove todo

--- src/main/java/com/ba/barcodereader/helper/ScannerHelper.java
+++ src/main/java/com/ba/barcodereader/helper/ScannerHelper.java
@@ -15,7 +15,7 @@ public class ScannerHelper {
             Process process = new ProcessBuilder(Config.SCANNER_EXE_PATH).start();
             process.waitFor(5, TimeUnit.SECONDS);
             new ProcessBuilder("taskkill /IM \"kodak.scan.exe\" /F").start();
-        } catch (FileNotFoundException e) {//TODO:Development[Throw custom exception]
+        } catch (FileNotFoundException e) {
             log.error("File not found! Full file path : {}", Config.SCANNER_EXE_PATH);
         } catch (IOException e) {
             log.error("Something went wrong while processing exe file! File path : {}", Config.SCANNER_EXE_PATH);
